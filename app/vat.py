3ec40601e007f19b2da791a7563dca0512424bc4
panxiangrong <panxiangrong@agora.io>
2026-09-10 15:01:46 +0800

    feat: 微调逻辑

--- app/vat.py
+++ app/vat.py
@@ -29,8 +29,11 @@ class Application(base.Application):
         else:
             doc = export_declaration_docs[0]
             if doc["status"] != "submitted":
-                diagnose["buyer_name"] = diagnose.get("buyer_name","") + "报关单未提交\n"
-                diagnose["buyer_tax_id"] = diagnose.get("buyer_tax_id","") + "报关单未提交\n"
+                pre_entry_number = header["extentions"]["pre_entry_number"]
+                diagnose["buyer_name"] = diagnose.get("buyer_name","") +\
+                    f"报关单未提交, 报关单预录入编码: {pre_entry_number}\n"
+                diagnose["buyer_tax_id"] = diagnose.get("buyer_tax_id","") +\
+                    f"报关单未提交, 报关单预录入编码: {pre_entry_number}\n"
             else:
                 doc_content = self.get_document_invoice_content(doc["email_id"], doc["filename"], doc["sheet"])
 
@@ -40,11 +43,11 @@ class Application(base.Application):
                 domestic_consignor_code = doc_content["header"]["extentions"]["domestic_consignor_code"]
                 if (self.normalize_text(buyer_name) != self.normalize_text(domestic_consignor)):
                     diagnose["buyer_name"] = diagnose.get("buyer_name","") +\
-                        f"销售方名称 {buyer_name} 与报关单境内发货人 {domestic_consignor} 不一致"
+                        f"销售方名称 {buyer_name} 与报关单境内发货人 {domestic_consignor} 不一致, 报关单预录入编码: {pre_entry_number}\n"
 
                 if buyer_tax_id.strip() != domestic_consignor_code.strip():
                     diagnose["buyer_tax_id"] = diagnose.get("buyer_tax_id","") +\
-                        f"销售方识别号 {buyer_tax_id} 与报关单境内发货人代码 {domestic_consignor_code} 不一致"
+                        f"销售方识别号 {buyer_tax_id} 与报关单境内发货人代码 {domestic_consignor_code} 不一致, 报关单预录入编码: {pre_entry_number}\n"
 
         return diagnose, {}
 
